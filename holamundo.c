37006eee38198d268bf66a4d6f3553c08c3fb723
Alejandroarana <6c.arana.chavez.alejandro@gmail.com>
2018-04-19 19:34:02 -0600

    la3
    otro comentario

--- holamundo.c
+++ holamundo.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 int main(int argc, char const *argv[]) {
-  printf("holamundo\n", );
+  int a=0;
+  printf("holamundo\n");
   return 0;
+  switch (case) {
+    case 1:
+    printf("caso1")
+    break;
+    default:
+    printf("opcion no valida");
+    break;
+  }
 }
